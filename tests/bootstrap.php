ab88fb5d29bf01fd47dcb5f5ce687f541fc86b9f
Guillermo A. Fisher <guillermoandraefisher@gmail.com>
2014-02-11 14:53:21 -0500

    Sorta working, added some stuff, moved stuff around.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,8 +6,5 @@ set_include_path(implode(PATH_SEPARATOR, array(
 	dirname(dirname(__FILE__)),
 )));
 
-// define the path to the fixtures directory
-define('GRIA_FIXTURE_DIR', dirname(__FILE__) . '/fixtures');
-
 // pull in composer dependencies
 require 'vendor/autoload.php';
