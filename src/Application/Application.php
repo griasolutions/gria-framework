e6ebbb26068103bd923fed6368f80ca7d4d1ee40
Guillermo A. Fisher <guillermoandraefisher@gmail.com>
2014-02-28 08:40:26 -0500

    Cleaned up the application kicker-offer a little bit.

--- src/Application/Application.php
+++ src/Application/Application.php
@@ -11,53 +11,65 @@ namespace Gria\Application;
 use \Gria\Config;
 use \Gria\Http;
 use \Gria\Controller;
-use \Gria\Helper;
 
+/**
+ * Performs initial checks and kicks off the MVC application. If any problems bubble up to this level, we simply stop
+ * script execution and tell the user what is going on.
+ */
 class Application
 {
 
-	use Config\ConfigAwareTrait, Helper\HelperManagerAwareTrait;
+	use Config\ConfigAwareTrait;
 
     /** @var \Gria\Controller\Dispatcher */
     private $_controllerDispatcher;
 
     /**
+     * Constructor
+     *
      * @param \Gria\Config\ConfigInterface $config
      */
 	public function __construct(Config\ConfigInterface $config)
 	{
-        $this->isValidEnvironment() || die('No application environment defined!');
-        $this->setConfig($config)->setHelperManager(new Helper\Manager($config));
+        $this->checkEnvironment();
+        $this->setConfig($config);
 	}
 
+    /**
+     * Checks the environment
+     *
+     * @return void
+     */
+    public function checkEnvironment()
+    {
+        defined('GRIA_ENV') || die('No application environment defined!');
+    }
+
 	/**
+     * Runs the controller dispatcher
+     *
 	 * @return void
 	 */
 	public function run()
 	{
-		$this->getControllerDispatcher()->run();
+		try {
+            $this->getControllerDispatcher()->run();
+        } catch (\Exception $ex) {
+            die($ex->getMessage());
+        }
 	}
 
     /**
+     * Retrieves an instance of the controller dispatcher
+     *
      * @return \Gria\Controller\Dispatcher
      */
     public function getControllerDispatcher()
     {
         if (!$this->_controllerDispatcher) {
-            $this->_controllerDispatcher = new Controller\Dispatcher(
-                $this->getConfig(),
-                $this->getHelperManager()
-            );
+            $this->_controllerDispatcher = new Controller\Dispatcher($this->getConfig());
         }
         return $this->_controllerDispatcher;
-    }
-
-    /**
-     * @return boolean
-     */
-    public function isValidEnvironment()
-    {
-        return defined('GRIA_ENV');
     }
 
 }
